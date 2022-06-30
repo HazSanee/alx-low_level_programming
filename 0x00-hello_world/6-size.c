bb40f7a4c1fb6ac7a5ee58cf00cbf94624d5e022
Hadiza sani <hadeexaahsanee@gmail.com>
2022-06-30 16:54:44 -0700

    sizenbytes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include<stdio>
+/**
+ * main -entry point
+ * Return: Alwats 0 (success)
+ */
+int main(void)
+{
+int a;
+long long int c;
+char d;
+float;
+long int b;
+printf("size of a char: %lu byte(s)\n", sizeof(d));
+printf("Size of an int: %lu byte(s)\n", sizeof(a));
+printf("Size of a long int: %lu byte(s)\n", sizeof(b));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(c));
+printf("Size of a float: %lu byte(s)\n", sizeof(int)); 
+return (0);
+}
