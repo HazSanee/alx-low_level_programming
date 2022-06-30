447e42802d5ccecc985786c1e32e4069ddc766d6
Hadiza sani <hadeexaahsanee@gmail.com>
2022-06-30 15:51:01 -0700

    return with a small r

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
+#include<string>
 /**
  * main-entry point
- * return: always 0 (success)
+ * Return:always 0 (success)
  */
+
 int main(void)
 {
-puts("\"programming is like building a multilingualpuzzle");
+char *s = "programming is like building a multilingual puzzle";
+puts(s);
 return (0);
 }
