a68fe4c42259ffdd0723e3e30c5248710d1654fc
Hadiza sani <hadeexaahsanee@gmail.com>
2022-07-05 14:01:01 -0700

    print alphabet 10 timess

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,9 @@ void print_alphabet_x10(void)
 	int i;
 	char j;
 
-	for (i = 1; 1 = <= 10; i++)
+	for (i = 1; 1 <= 10; i++)
 	{
-		for (j = 'a'; j = <= 'z'; j++)
+		for (j = 'a'; j <= 'z'; j++)
 		{
 			_putchar(j);
 			_putchar('\n');
