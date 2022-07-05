f30c068d2fe519835dd21f87ee709bca0f35d7fa
Hadiza sani <hadeexaahsanee@gmail.com>
2022-07-05 14:32:18 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha -print alphabets
+ *
+ * Return:
+ */
+
+int _isalpha(int c)
+
+{
+	if(c >= 'a'; && c <= 'z'; || C >= 'A'; && C <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
