593e29870c1b2c06b0b59aac3c0312f7c39c66bb
Hadiza sani <hadeexaahsanee@gmail.com>
2022-07-05 13:27:59 -0700

    void return

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_alphabet->return alphabet
+ */
+
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+	_putchar(x);
+	}
+	_putchar('\n');
+}
