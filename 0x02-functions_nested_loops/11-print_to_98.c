667e38f86959e5607002fcc05d4a2ad95d51f6fd
Hadiza sani <hadeexaahsanee@gmail.com>
2022-07-06 10:31:20 -0700

    naturales

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,9 +10,9 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 		for (n = n; n <= 97; n++)
-		printf("%d, ", n);
-		else
+			printf("%d, ", n);
+	else
 		for (n = n; n > 98; n--)
 			printf("%d, ", n);
-			printf("98\n");
+	printf("98\n");
 }
