41dec397c00e96d498f4dd4450d25db693d55326
Hadiza sani <hadeexaahsanee@gmail.com>
2022-07-06 05:15:21 -0700

    print lower case

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,4 +1,20 @@
 #include "main.h"
 
 /**
- * main - 
+ * _islower - check for alphabetical characters
+ * @c: variable
+ * Return: Returns 1 if c is lowercase
+ * Returns 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
