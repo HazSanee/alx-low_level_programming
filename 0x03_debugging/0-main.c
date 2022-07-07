18be82148c915e428aee79982406d8a100400441
Hadiza sani <hadeexaahsanee@gmail.com>
2022-07-07 06:40:23 -0700

    negative and posiive

--- 0x03_debugging/0-main.c
+++ 0x03_debugging/0-main.c
@@ -1,10 +1,7 @@
 #include "main.h"
 
-/*
- * main - Entry point
- *
- * Description: tests function that prints if integer is positive or negative
- *
+/**
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 
