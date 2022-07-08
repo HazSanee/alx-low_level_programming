92eb470bf061df153a34faacef48e0a77a5400d7
Hadiza sani <hadeexaahsanee@gmail.com>
2022-07-08 15:34:00 -0700

    hot fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,22 +1,21 @@
 #include "main.h"
 
 /**
- * print_most_numbers - a function thattprints the numbers from 0 to 9
- * Return: 0-9, excluding 2 and 4 followed by a new line
+ * print_most_numbers - print 0-9 without 2 and 4
+ * Return: nothing
  */
 
 void print_most_numbers(void)
 {
-	int x;
+	char i = '0';
 
-	for (i = 0; i <= 9; i++)
+	while (i <= '9')
 	{
-		if ((i == 2) || (i == 4))
-			continue;
-
-		else
-			_putchar(i + '0');
+		if ((i != '2') && (i != '4'))
+		{
+			_putchar(i);
+		}
+		i++;
 	}
 	_putchar('\n');
 }
-
