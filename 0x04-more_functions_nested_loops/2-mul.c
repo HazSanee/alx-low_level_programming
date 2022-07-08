06349957721fffce3840a2a1c375a48c367d4012
Hadiza sani <hadeexaahsanee@gmail.com>
2022-07-07 22:09:11 -0700

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: product of both int
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,7 +4,10 @@
  * _putchar - writes the character c to stdout
  * _isupper - checks if c is capital letter
  * _isdigit - checks if value is a digit 0-9
+ * mul(int a, int b) - multiplies two integers
  */
 
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
+
