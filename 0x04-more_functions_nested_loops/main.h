fddd456197b7fde658d3384afc57c906221ad3dc
Hadiza sani <hadeexaahsanee@gmail.com>
2022-07-07 22:26:52 -0700

    print

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,7 +5,7 @@
  * _isupper - checks if c is capital letter
  * _isdigit - checks if value is a digit 0-9
  * mul(int a, int b) - multiplies two integers
- * print_numbers - prints numbers 0-9 followed by a new line
+ * print_numbers - a function that prints numbers 0-9
  */
 
 int _isupper(int c);
