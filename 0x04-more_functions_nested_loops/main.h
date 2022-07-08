4320a088ddb4bd9e0789864926c1af6b08a175d5
Hadiza sani <hadeexaahsanee@gmail.com>
2022-07-08 15:12:33 -0700

    hot ix

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,15 +1,7 @@
-#include <unistd.h>
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * _putchar - writes the character c to stdout
- * _isupper - checks if c is capital letter
- * _isdigit - checks if value is a digit 0-9
- * mul(int a, int b) - multiplies two integers
- * print_numbers - a function that prints numbers 0-9
- * print_most_numbers - a function that prints the numbers, from 0 to 9
- * more_numbers - prints 10 times the numbers, from 0 to 14
- */
-
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -17,3 +9,4 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 
+#endif /* MAIN_H */
