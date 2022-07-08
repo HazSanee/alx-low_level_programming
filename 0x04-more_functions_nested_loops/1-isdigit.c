bd9cadcae0bd9bdb016b631d210f998e8d55f5ee
Hadiza sani <hadeexaahsanee@gmail.com>
2022-07-08 15:18:46 -0700

    hot fix

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,14 +5,11 @@
  * @c: int to be checked
  * Return: 1 is c is a digit otherwise 0
  */
-int _isdigit(int c);
+
+int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
-	{
-	return (1);
-	}
+		return (1);
 	else
-	{
-	return (0);
-	}
+		return (0);
 }
