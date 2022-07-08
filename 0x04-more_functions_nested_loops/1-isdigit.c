6b6e02355dd2dc46ea1f5c04f97bf1fe50e76306
Hadiza sani <hadeexaahsanee@gmail.com>
2022-07-07 21:56:17 -0700

    digit0-9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if value is a digit 0-9
+ * @c: int to be checked
+ * Return: 1 is c is a digit otherwise 0
+ */
+int _isdigit(int c);
+{
+	if (c >= '0' && c <= '9')
+	return (1);
+	else
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,6 +3,8 @@
 /**
  * _putchar - writes the character c to stdout
  * _isupper - checks if c is capital letter
+ * _isdigit - checks if value is a digit 0-9
  */
 
 int _isupper(int c);
+int _isdigit(int c);
