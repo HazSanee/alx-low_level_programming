e154f07d6d4d969e15a2bcf91014972c9c16c3b0
HazSanee <106750412+HazSanee@users.noreply.github.com>
2022-07-22 23:34:19 +0100

    Update 2-strlen.c

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
 /**
- *  * _strlen - Write a function that returns the length of a string.
- *   *
- *    * @s: the variable pointer at s
- *     *
- *      * Return: Always return count
- *       */
+ * _strlen - Write a function that returns the length of a string.
+ * @s: the variable pointer at s
+ * Return: Always return count
+ */
 
 int _strlen(char *s)
 {
