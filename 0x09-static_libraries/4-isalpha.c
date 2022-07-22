e89733b9a9638a898d15fd4f9fb91cbc7edadea5
Hadiza sani <hadeexaahsanee@gmail.com>
2022-07-22 15:22:34 -0700

    my first library

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha -print alphabets
+ * @c: variable
+ * Return: 1 if c is a letter, lowercase or uppercase
+ * Returns: 0 otherwise
+ *
+ */
+
+int _isalpha(int c)
+
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - print absolute values
+ * @a: absolute value
+ * Return: absolute value
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+	{
+	a = a * -1;
+	}
+	return (a);
+}
+
